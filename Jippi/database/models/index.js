c456df97997901934146e0ed9e3143e885be7781
ezzineog <ezzineahmedtaha@gmail.com>
2020-10-19 14:06:28 +0200

    befor pulling

--- Jippi/database/models/index.js
+++ Jippi/database/models/index.js
@@ -3,7 +3,7 @@
 const dbConfig = require("../config/db.config.js");
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("", dbConfig.USER, dbConfig.PASSWORD, {
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
   dialect: dbConfig.dialect,
   operatorsAliases: false,
