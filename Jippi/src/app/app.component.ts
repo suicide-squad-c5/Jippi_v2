79a3edcf53d273cf7e807c1347093d99d5f30045
GhassenEljday <ghassenjday2001@gmail.com>
2020-10-28 22:31:25 +0100

    item detail fix

--- Jippi/src/app/app.component.ts
+++ Jippi/src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Jippi';
+
+  ngOnInit(): void {
+    this.warning();
+  }
+  warning() {
+    console.log('%c WARNING', 'color: red; font-size:50px');
+    console.log(
+      '%c If somone told to open the console and put some script! just stop maybe he will effe in your account',
+      'color: #E5E7E9; -webkit-text-stroke: 0.3px black; -webkit-text-fill-color: white; ; font-size:16px'
+    );
+  }
 }
