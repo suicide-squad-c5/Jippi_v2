1fdcd779a30868cf080778c34b5632a1c86bd921
achref-bououn <achref.bououn@gmail.com>
2020-11-04 11:37:23 +0100

    pdf converter

--- Jippi/src/app/user/customer/snippet/snippet.component.ts
+++ Jippi/src/app/user/customer/snippet/snippet.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import jspdf from 'jspdf';
+import html2canvas from 'html2canvas';
 @Component({
   selector: 'app-snippet',
   templateUrl: './snippet.component.html',
@@ -19,5 +20,22 @@ export class SnippetComponent implements OnInit {
     console.log(history.state.data[0].total);
     this.total = history.state.data[0].total;
     this.data = history.state.data;
+  }
+
+  public captureScreen() {
+    var data = document.getElementById('contentToConvert');
+    console.log('SnippetComponent -> captureScreen -> data', data);
+    html2canvas(data).then((canvas) => {
+      var imgWidth = 208;
+      var pageHeight = 295;
+      var imgHeight = (canvas.height * imgWidth) / canvas.width;
+      var heightLeft = imgHeight;
+
+      const contentDataURL = canvas.toDataURL('image/png');
+      let pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF
+      var position = 0;
+      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
+      pdf.save(`${this.key}.pdf`); // Generated PDF
+    });
   }
 }
